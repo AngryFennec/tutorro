d2fb2c5d65c30da533ed8fdfea49079c2af736e2
Angry Fennec <shellfix@gmail.com>
2021-12-01 18:55:24 +0300

    Предметы

--- pages/Academic/Academic.ts
+++ pages/Academic/Academic.ts
@@ -1,4 +1,23 @@
 import {Component, Vue} from "nuxt-property-decorator";
 
+type AcademicType = {
+  id: number;
+  lesson: string;
+};
+
 @Component
-export default class Academic extends Vue {}
+export default class Academic extends Vue {
+
+  public get academics(): AcademicType[] {
+    return [
+      {
+        id: 1,
+        lesson: "L1",
+      },
+      {
+        id: 2,
+        lesson: "L2",
+      }
+    ];
+  }
+}
